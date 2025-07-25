3cf03e9d5faf10a04ee7ec99c1d90fd3ed01f707
Rehnuma111 <rehnumabano1111@gmail.com>
2025-07-25 18:09:15 +0530

    add hosted url

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,10 @@
-export const  USER_API_ENDPOINT = "http://localhost:3000/api/v1/users"
-export const  JOB_API_ENDPOINT = "http://localhost:3000/api/v1/job"
-export const  APPLICATION_API_ENDPOINT = "http://localhost:3000/api/v1/application"
-export const  COMPANY_API_ENDPOINT = "http://localhost:3000/api/v1/company"
+// export const  USER_API_ENDPOINT = "http://localhost:3000/api/v1/users"
+// export const  JOB_API_ENDPOINT = "http://localhost:3000/api/v1/job"
+// export const  APPLICATION_API_ENDPOINT = "http://localhost:3000/api/v1/application"
+// export const  COMPANY_API_ENDPOINT = "http://localhost:3000/api/v1/company"
+
+
+export const  USER_API_ENDPOINT = "https://job-portal-app-backend-1-ghf0.onrender.com/api/v1/users"
+export const  JOB_API_ENDPOINT = "https://job-portal-app-backend-1-ghf0.onrender.com/api/v1/job"
+export const  APPLICATION_API_ENDPOINT = "https://job-portal-app-backend-1-ghf0.onrender.com/api/v1/application"
+export const  COMPANY_API_ENDPOINT = "https://job-portal-app-backend-1-ghf0.onrender.com/api/v1/company"
